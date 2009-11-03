227e055fe834657b79f65895af6a51a1c66f13c2
Paul Miller <paul.w.miller@wmich.edu>
2009-11-03 01:51:47 -0500

    New version

--- graph.py
+++ graph.py
@@ -1,23 +1,20 @@
-class Vertex (object):
-    def __init__(self, value):
-	self.value = value
-
-    def __str__(self):
-	return self.value.__str__()
-
-    def __cmp__(self, other):
-	if other.__class__ == self.__class__:
-	    return cmp (self.value, other.value)
-	else:
-	    return cmp (self.value, other)
-
-class Edge (object):
-    def __init__(self, head, tail, directed=False):
-	self.head = head
-	self.tail = tail
-	self.directed = directed
-
 class Graph (object):
-    def __init__(self, vertices = None, edges = None):
-	pass
+    def __init__(self, vertices = None, edges = None, directed = False):
+	if vertices is None:
+	    vertices = []
+	if edges is None:
+	    edges = []
 
+	self.directed = directed
+	self.vertices = list (set (vertices))
+
+	for e in edges:
+	    if len (e) != 2 or \
+		   e[0] not in self.vertices or e[1] not in self.vertices:
+		raise TypeError, "%(edge)s is not a valid edge." % \
+		      { 'edge' : repr (e) } 
+
+	if directed:
+	    self.edges = list (set ([tuple (e) for e in edges]))
+	else:
+	    self.edges = list (set ([frozenset (e) for e in edges]))
