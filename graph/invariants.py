1a611bcaf4c0ad52ed4a9b09f14b3f258d99f5e8
Paul Miller <paul.w.miller@wmich.edu>
2009-11-16 21:19:16 -0500

    Doc strings

--- graph/invariants.py
+++ graph/invariants.py
@@ -1,18 +1,37 @@
+'''
+A \emph {graph invariant} is a function that will return the same value when
+passed different, but isomorphic graphs as parameters.  This module implements
+several common graph invariants.
+'''
+
 from algorithms import DFS
 
 def degrees (G):
-    return [ len ([e for e in G.edges if v in e]) for v in G.vertices]
+    '''
+    Returns the degree of each vertex of $G$, in no particular order.
+    '''
+    return set ([len ([e for e in G.edges if v in e]) for v in G.vertices])
 
 def degreeSequence (G):
-    return sorted (degrees (G))
+    return sorted (list (degrees (G)))
 
 def minDegree (G):
+    '''
+    Returns the smallest degree of any vertex in the vertex set of $G$.
+    '''
     return min (degrees (G))
 
 def maxDegree (G):
+    '''
+    Returns the largest degree of any vertex in the vertex set of $G$.
+    '''
     return max (degrees (G))
 
 def is_regular (G):
+    '''
+    Returns True if and only if every vertex of $G$ has the same degree,
+    otherwise returns False.
+    '''
     return minDegree (G) == maxDegree (G)
 
 def components (G):
@@ -23,9 +42,15 @@ def components (G):
         V = [v for v in V if v not in component]
 
 def is_connected (G):
+    '''
+    Returns True if and only if $G$ is connected, otherwise returns False.
+    '''
     return len (list (DFS (G, G.vertices[0]))) == len (G.vertices)
 
 def is_tree (G):
+    '''
+    Returns True if and only if $G$ is a tree, otherwise returns False.
+    '''
     return is_connected (G) and len (G.edges) == len (G.vertices) - 1
 
 def vertexConnectivity (G):
